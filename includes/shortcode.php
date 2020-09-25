75df79d91b6e4fbf626a340ac33bfd80ebf5293a
Rover <rover@MacBook-Pro.local>
2020-09-25 22:23:27 +0800

    $wpt_table_row_serial

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -1146,6 +1146,7 @@ function wpt_table_row_generator( $table_row_generator_array ){
                 $wpt_start_point .= "' id='radio_id_{$temp_number}_" . $data['id'];
                 $wpt_start_point .= "' data-temp_number='{$temp_number}' data-product_id='" . $data['id']; 
                 $wpt_start_point .= "' class='" . ( ( $table_type == 'normal_table' && $product_type == 'grouped' ) || $product_type == 'variable' || $product_type == 'external' || ( $data['stock_status'] != 'instock' && $data['stock_status'] != 'onbackorder' ) ? 'disabled' : 'enabled' );
+                $wpt_start_point .= " wpt_table_row_serial=" . $wpt_table_row_serial;
                 $wpt_start_point .= " wpt_radio wpt_td_radio wpt_radio_temp_{$temp_number}_pr_" . $data['id'] . " wpt_radio_{$temp_number} wpt_inside_radio_{$temp_number}'";
                 if ($wpt_table_row_serial == 0) {
                     $wpt_start_point .= " checked='true'";
