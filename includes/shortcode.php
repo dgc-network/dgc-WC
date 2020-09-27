250db487cdc8824e54ea6ef2f0294c5238c321af
Rover <rover@MacBook-Pro.local>
2020-09-27 17:06:58 +0800

    $( function() {     $( "#datepicker" ).datepicker();     $( "#format" ).on( "change", function() {       $( "#datepicker" ).datepicker( "option", "dateFormat", $( this ).val() );     });   } );

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -332,10 +332,10 @@ function wpt_shortcode_generator( $atts = false ) {
         $html_start_date .= "<div>";
         $html_start_date .= "<label for='start_date_header_section_text'><h5>{$start_date_header_section_text}</h5></lable>";
         $html_start_date .= "<input id='datepicker' type='text' name='start_date_header_section' value='{$default_start_date}'>";
-        //$html_start_date .= "<script>";
-        //$html_start_date .= "$( '#datepicker' ).datepicker();";
+        $html_start_date .= "<script>";
+        $html_start_date .= "$( '#datepicker' ).datepicker();";
         //$html_start_date .= "$( '#datepicker' ).datepicker( 'option', 'dateFormat', 'yy-mm-dd' );";
-        //$html_start_date .= "</script>";
+        $html_start_date .= "</script>";
         $html_start_date .= "</div>";
     }
     
