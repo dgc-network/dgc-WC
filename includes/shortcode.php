65238806130f97bd1bfd99a543a78d2f928d7a67
roverchen <rover.k.chen@gmail.com>
2020-09-30 14:15:07 +0800

    $wpt_start_date .= "var x = document.getElementById('startdate{$wpt_table_row_serial}');";

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -1189,9 +1189,8 @@ function wpt_table_row_generator( $table_row_generator_array ){
                 $wpt_start_date .= "<script>";
                 $wpt_start_date .= "var x = document.getElementById('startdate{$wpt_table_row_serial}');";
                 $wpt_start_date .= "var y = document.getElementById('datepicker').value;";
-                $wpt_start_date .= "var z = Date.parse(y)";
-                $wpt_start_date .= "z.setDate(z.getDate() + {$wpt_table_row_serial})";
-                //$wpt_start_date .= "y = y+{$wpt_table_row_serial};";
+                $wpt_start_date .= "var z = Date.parse(y);";
+                $wpt_start_date .= "z.setDate(z.getDate() + {$wpt_table_row_serial});";
                 $wpt_start_date .= "x.innerHTML = z;";
                 $wpt_start_date .= "</script>";
 
