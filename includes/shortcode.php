d4dd14ff2f3346a51f5cb53f78e5bdbc541d7409
Rover <rover@MacBook-Pro.local>
2020-09-30 20:36:50 +0800

    new Date(

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -1190,9 +1190,9 @@ function wpt_table_row_generator( $table_row_generator_array ){
                 $wpt_start_date .= "var y = document.getElementById('datepicker').value;";
                 $wpt_start_date .= "var z = new Date(y);";
                 //$wpt_start_date .= "var z = new Date(Date.parse(y) * 1000);";
-                //$wpt_start_date .= "z.setDate(z.getDate() + {$wpt_table_row_serial});";
+                $wpt_start_date .= "d = z.getDate() + {$wpt_table_row_serial};";
                 $wpt_start_date .= "var x = document.getElementById('startdate{$wpt_table_row_serial}');";
-                $wpt_start_date .= "x.innerHTML = z;";
+                $wpt_start_date .= "x.innerHTML = d;";
                 $wpt_start_date .= "</script>";
 
                 $wpt_each_row['start_date'] = $wpt_start_date;
