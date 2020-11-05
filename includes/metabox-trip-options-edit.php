aa6f77fc002d1a8166f6b489a87d70d072e20d63
Rover <rover@MacBook-Pro.local>
2020-11-05 23:35:23 +0800

    </table>

--- includes/metabox-trip-options-edit.php
+++ includes/metabox-trip-options-edit.php
@@ -263,8 +263,8 @@ class Metabox_Trip_Options_Edit {
 	
 		<style>
   			#sortable { list-style-type: none; margin: 0; padding: 0; width: 100%; }
-  			#sortable li { background: #f2f2f2; border: 1px solid #ccc; margin: 0 3px 3px 3px; padding: 0.4em; padding-left: 1.5em; font-size: 1.4em; height: 18px;
-			  	display: inline-block; zoom: 1; *display: inline; list-style-type: none; vertical-align: middle;}
+  			#sortable li { background: #f2f2f2; border: 1px solid #ccc; margin: 0 3px 3px 3px; padding: 0.4em; padding-left: 1.5em; font-size: 1.4em; height: 18px;}
+/*			  	display: inline-block; zoom: 1; *display: inline; list-style-type: none; vertical-align: middle;}*/
 			#sortable li span { position: absolute; margin-left: -1.3em; }
 			#sortable div { background: #f2f2f2; border: 1px solid #ccc;}
 			#sortable li.active { background: #FFFFFF; height: 500px; }
