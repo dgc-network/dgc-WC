1c252d4e4c76fadd0bf5a7b25218e91678fb206a
roverchen <rover.k.chen@gmail.com>
2020-11-06 13:40:44 +0800

    form-table

--- includes/metabox-trip-options-edit.php
+++ includes/metabox-trip-options-edit.php
@@ -277,7 +277,8 @@ class Metabox_Trip_Options_Edit {
 							//$( this ).html("Day X, My plan");
 							$( this ).on("click", function() {
 								$( this ).toggleClass('active');
-								$( this ).children('form-table').off(); 
+								//$( this ).children('form-table').off(); 
+								$( this ).off( ".form-table" );
 							});
 							return false;
 						};
