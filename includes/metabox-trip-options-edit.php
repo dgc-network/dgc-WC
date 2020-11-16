c4538d6cb76e107d6647957086a6a3da8d1f4ec8
roverchen <rover.k.chen@gmail.com>
2020-11-16 14:15:49 +0800

    $itineraries[$x]['title'] !=

--- includes/metabox-trip-options-edit.php
+++ includes/metabox-trip-options-edit.php
@@ -203,7 +203,9 @@ class Metabox_Trip_Options_Edit {
 		<?php 
 		if ( is_array( $itineraries ) && count( $itineraries ) > 0 ) {
 			foreach ( $itineraries as $itinerary ) {
-				$xx++;
+				if (($itineraries[$x]['title'] != __( 'Day X, My plan', 'wp-travel' )) || ($itineraries[$x]['title'] != "")) {
+					$xx++;
+				}
 			}
 		} else {?>
 			<tr class="no-itineraries"><td>
