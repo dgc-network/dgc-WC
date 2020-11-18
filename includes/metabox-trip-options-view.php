e20ab5b5c83aa4c1fac8289d1e5818c372eee8d7
roverchen <rover.k.chen@gmail.com>
2020-11-18 15:15:49 +0800

    'Misc. Options', 'wp-travel'

--- includes/metabox-trip-options-view.php
+++ includes/metabox-trip-options-view.php
@@ -1,129 +1,69 @@
 <?php
 class Metabox_Trip_Options_View {
 	public static function init() {
-		add_action( 'add_meta_boxes', array( __CLASS__, 'register_meta_box' ) );
-		//add_action( 'admin_menu', 'trip_add_metabox' );
-		add_action( 'admin_menu', array( __CLASS__, 'trip_add_metabox' ) );
-		//add_action( 'save_post', 'trip_save_meta', 10, 2 );
-		add_action( 'save_post', array( __CLASS__, 'trip_save_metabox' ) );
+		add_filter( 'woocommerce_product_tabs', array( __CLASS__, 'woo_new_product_tab' ) );
 	}
 /*
-	public static function register_meta_box() {
-		$settings        = wp_travel_get_settings();
-		$switch_to_react = $settings['wp_travel_switch_to_react'];
-		if ( 'yes' === $switch_to_react ) {
-			add_meta_box( 
-				'wp-travel-trip-options', 
-				esc_html__( 'Trip Options', 'wp-travel' ), 
-				array( __CLASS__, 'meta_box_callback' ), // callback function
-				WP_TRAVEL_POST_TYPE, 
-				'advanced', 
-				'high' 
-			);
-		}
-	}
+	<li><a href="#tab1"><?php esc_html_e( 'Itinerary', 'wp-travel' ); ?></a></li>
+	<li><a href="#tab2"><?php esc_html_e( 'Prices & Dates', 'wp-travel' ); ?></a></li>
+	<li><a href="#tab3"><?php esc_html_e( 'Includes/Excludes', 'wp-travel' ); ?></a></li>
+	<li><a href="#tab4"><?php esc_html_e( 'Facts', 'wp-travel' ); ?></a></li>
+	<li><a href="#tab5"><?php esc_html_e( 'Gallery', 'wp-travel' ); ?></a></li>
+	<li><a href="#tab6"><?php esc_html_e( 'Locations', 'wp-travel' ); ?></a></li>
+	<li><a href="#tab7"><?php esc_html_e( 'FAQs', 'wp-travel' ); ?></a></li>
+	<li><a href="#tab8"><?php esc_html_e( 'Misc. Options', 'wp-travel' ); ?></a></li>
+	<li><a href="#tab9"><?php esc_html_e( 'Tabs', 'wp-travel' ); ?></a></li>
 */
-	public static function meta_box_callback() {
-		echo '<div id="wp-travel-trip-options-wrap"></div>';
-	}
-
-	/**
-	 * Add a new meta box for product
-	 * Step 1. add_meta_box()
-	 * Step 2. Callback function with meta box HTML
-	 * Step 3. Save meta box data
-	 */
-	function trip_add_metabox() {
-/*		
-		add_meta_box(
-			'trip_metabox', // metabox ID
-			'Itineraries', // title
-			//'trip_metabox_callback', // callback function
-			array( __CLASS__, 'trip_metabox_callback' ), // callback function
-			'product', // post type or post types in array
-			'normal', // position (normal, side, advanced)
-			'default' // priority (default, low, high, core)
+	function woo_new_product_tab( $tabs ) {
+		// Adds the new tab
+		$tabs['itineraries_tab'] = array(
+			'title' 	=> __( 'Itineraries', 'woocommerce' ),
+			'priority' 	=> 50,
+			'callback' 	=> 'woo_new_product_tab_content'
 		);
-*/		
+		$tabs['prices_date_tab'] = array(
+			'title' 	=> __( 'Prices & Dates', 'wp-travel' ),
+			'priority' 	=> 50,
+			'callback' 	=> 'woo_new_product_tab_content'
+		);
+		$tabs['includes_excludes_tab'] = array(
+			'title' 	=> __( 'Includes/Excludes', 'wp-travel' ),
+			'priority' 	=> 50,
+			'callback' 	=> 'woo_new_product_tab_content'
+		);
+		$tabs['facts_tab'] = array(
+			'title' 	=> __( 'Facts', 'wp-travel' ),
+			'priority' 	=> 50,
+			'callback' 	=> 'woo_new_product_tab_content'
+		);
+		$tabs['gallery_tab'] = array(
+			'title' 	=> __( 'Gallery', 'wp-travel' ),
+			'priority' 	=> 50,
+			'callback' 	=> 'woo_new_product_tab_content'
+		);
+		$tabs['locations_tab'] = array(
+			'title' 	=> __( 'Locations', 'wp-travel' ),
+			'priority' 	=> 50,
+			'callback' 	=> 'woo_new_product_tab_content'
+		);
+		$tabs['faqs_tab'] = array(
+			'title' 	=> __( 'FAQs', 'wp-travel' ),
+			'priority' 	=> 50,
+			'callback' 	=> 'woo_new_product_tab_content'
+		);
+		$tabs['misc_options_tab'] = array(
+			'title' 	=> __( 'Misc. Options', 'wp-travel' ),
+			'priority' 	=> 50,
+			'callback' 	=> 'woo_new_product_tab_content'
+		);
+		return $tabs;
 	}
- 
-	function trip_metabox_callback( $post ) {
 
-		/**
-		 * Retrieves a post meta field for the given post ID.
-		 * get_post_meta( int $post_id, string $key = '', bool $single = false )
-		 */
-		$seo_title = get_post_meta( $post->ID, 'seo_title', true );
-		$seo_robots = get_post_meta( $post->ID, 'seo_robots', true );
- 
-		// nonce, actually I think it is not necessary here
-		wp_nonce_field( 'somerandomstr', '_tripnonce' );
- 
-		echo '<table class="form-table">
-			<tbody>
-				<tr>
-					<th><label for="seo_title">SEO title</label></th>
-					<td><input type="text" id="seo_title" name="seo_title" value="' . esc_attr( $seo_title ) . '" class="regular-text"></td>
-				</tr>
-				<tr>
-					<th><label for="seo_tobots">SEO robots</label></th>
-					<td>
-						<select id="seo_robots" name="seo_robots">
-							<option value="">Select...</option>
-							<option value="index,follow"' . selected( 'index,follow', $seo_robots, false ) . '>Show for search engines</option>
-							<option value="noindex,nofollow"' . selected( 'noindex,nofollow', $seo_robots, false ) . '>Hide for search engines</option>
-						</select>
-					</td>
-				</tr>
-			</tbody>
-		</table>';
- 
-	}
-
-	function trip_save_metabox( $post_id ) {
-	//public static function trip_save_metabox( $post_id, $post ) {
-	/*
-		// nonce check
-		if ( ! isset( $_POST[ '_tripnonce' ] ) || ! wp_verify_nonce( $_POST[ '_tripnonce' ], 'somerandomstr' ) ) {
-			return $post_id;
-		}
-
-		// check current use permissions
-		$post_type = get_post_type_object( $post->post_type );
-
-		if ( ! current_user_can( $post_type->cap->edit_post, $post_id ) ) {
-			return $post_id;
-		}
-
-		// Do not save the data if autosave
-		if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) {
-			return $post_id;
-		}
-
-		// define your own post type here
-		if( $post->post_type != 'product' ) {
-			return $post_id;
-		}
-	*/
-		/**
-		 * Updates a post meta field based on the given post ID.
-		 * update_post_meta( int $post_id, string $meta_key, mixed $meta_value, mixed $prev_value = '' )
-		 */
-
-		if( isset( $_POST[ 'seo_title' ] ) ) {
-			update_post_meta( $post_id, 'seo_title', sanitize_text_field( $_POST[ 'seo_title' ] ) );
-		} else {
-			delete_post_meta( $post_id, 'seo_title' );
-		}
-
-		if( isset( $_POST[ 'seo_robots' ] ) ) {
-			update_post_meta( $post_id, 'seo_robots', sanitize_text_field( $_POST[ 'seo_robots' ] ) );
-		} else {
-			delete_post_meta( $post_id, 'seo_robots' );
-		}
-
-		return $post_id;
-	}
+	function woo_new_product_tab_content() {
+		// The new tab content
+		echo '<h2>New Product Tab</h2>';
+		echo '<p>Here\'s your new product tab.</p>';
+	}	
 }
 
 Metabox_Trip_Options_View::init();
@@ -131,7 +71,7 @@ Metabox_Trip_Options_View::init();
 /**
  * Add a custom product data tab
  */
-add_filter( 'woocommerce_product_tabs', 'woo_new_product_tab' );
+//add_filter( 'woocommerce_product_tabs', 'woo_new_product_tab' );
 function woo_new_product_tab( $tabs ) {
 	// Adds the new tab
 	$tabs['itinerary_tab'] = array(
