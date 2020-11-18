1c30ec12a1bf4c30ae982989b56e17eac9670d1a
roverchen <rover.k.chen@gmail.com>
2020-11-18 11:27:16 +0800

    class="remove-itinerary"

--- includes/metabox-trip-options-edit.php
+++ includes/metabox-trip-options-edit.php
@@ -257,7 +257,7 @@ class Metabox_Trip_Options_Edit {
 						</tr>
 						<tr>
 							<td></td>
-							<td style="text-align:right"><button class="remove-itinerary" id="remove-itinerary-' . $x . '" style="color:red" type="button">' . $remove_itinerary . '</button></td>
+							<td class="remove-itinerary" style="text-align:right"><button id="remove-itinerary-' . $x . '" style="color:red" type="button">' . $remove_itinerary . '</button></td>
 						</tr>
 				  	  </tbody>
 					</table>
