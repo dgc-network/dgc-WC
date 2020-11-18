315b288a6a78e8281f878f9cdcb88a445aece15d
Rover <rover@MacBook-Pro.local>
2020-11-19 00:05:08 +0800

    require_once BASE_DIR . 'wp-travel/inc/payments/wp-travel-payments.php';

--- dgc-WC.php
+++ dgc-WC.php
@@ -57,14 +57,14 @@ if ( is_plugin_active( 'woocommerce/woocommerce.php' ) ) {
     require_once BASE_DIR . 'includes/metabox-trip-options-view.php';
     require_once BASE_DIR . 'wp-travel/inc/helpers.php';
     require_once BASE_DIR . 'wp-travel/inc/class-itinerary.php';
+    require_once BASE_DIR . 'wp-travel/inc/email-template-functions.php';
+    require_once BASE_DIR . 'wp-travel/inc/payments/wp-travel-payments.php';
 /*    
     require_once BASE_DIR . 'wp-travel/wp-travel.php';
     require_once BASE_DIR . 'wp-travel/inc/class-assets.php';
-    require_once BASE_DIR . 'wp-travel/inc/email-template-functions.php';
     require_once BASE_DIR . 'wp-travel/inc/helpers/helpers-price.php';
     require_once BASE_DIR . 'wp-travel/inc/helpers/helpers-fontawesome.php';
     require_once BASE_DIR . 'wp-travel/inc/admin/class-admin-metaboxes.php';
-    require_once BASE_DIR . 'wp-travel/inc/payments/wp-travel-payments.php';
     require_once BASE_DIR . 'wp-travel/core/helpers/trips.php';
     require_once BASE_DIR . 'wp-travel/core/helpers/trip-extras.php';
     require_once BASE_DIR . 'wp-travel/core/helpers/pricings.php';
