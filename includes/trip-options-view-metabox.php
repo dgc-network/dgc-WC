33ab193151574731bf2d31a5c79e66f14a85d6a1
Rover <rover@MacBook-Pro.local>
2020-12-05 10:05:36 +0800

    ?>

--- includes/trip-options-view-metabox.php
+++ includes/trip-options-view-metabox.php
@@ -61,16 +61,9 @@ class Trip_Options_View_Metabox {
 		echo '<p>Here\'s your new product tab.</p>';
 	}	
 
-	function itineraries_tab_content( $post ) {
-		if ( ! $post ) {
-			return;
-		}
-		$trip_code = wp_travel_get_trip_code( $post->ID );
-		//$itineraries = get_post_meta( $post->ID, 'wp_travel_trip_itinerary_data', true );
-		//$itineraries = WP_Travel_Helpers_Trips::get_trip( $post->ID );
-		//$itineraries = wp_travel_get_itineraries_array();
-
+	function itineraries_tab_content() {
 		$post_id = get_the_ID();
+		$trip_code = wp_travel_get_trip_code( $post_id );
 		$itineraries = get_post_meta( $post_id, 'wp_travel_trip_itinerary_data', true );
 		echo '$post->ID = ' . $post_id;
 		echo '{';
@@ -83,6 +76,38 @@ class Trip_Options_View_Metabox {
 			}
 		echo '}';
 		?>
+		<?php esc_html_e( 'Trip Code : ', 'wp-travel' ); ?>
+		<input type="text" id="wp-travel-trip-code" disabled="disabled" value="<?php echo esc_attr( $trip_code ); ?>" />
+		<br>
+		<h3><?php esc_html_e( 'Itinerary', 'wp-travel' ); ?></h3><?php
+		if ( is_array( $itineraries ) && count( $itineraries ) > 0 ) {
+			foreach ( $itineraries as $x=>$itinerary ) {
+				echo '
+				<tr><td>
+				<table class="update-itinerary" style="width:100%">
+					<tbody>
+					<tr>
+						<th>Itinerary title</th>
+						<td><input type="text" class="item-title" name="itinerary_item_title-' . $x . '" value="' . esc_attr( $itineraries[$x]['title'] ) . '" class="regular-text"></td>
+					</tr>
+					<tr>
+						<th>Itinerary description</th>
+						<td><textarea rows="3" name="itinerary_item_desc-' . $x . '" class="regular-text">' . esc_attr( $itineraries[$x]['desc'] ) . '</textarea></td>
+					</tr>
+					<tr>
+						<th>Itinerary date</th>
+						<td><input type="text" class="itinerary_item_date" name="itinerary_item_date-' . $x . '" value="' . esc_attr( $itineraries[$x]['date'] ) . '" class="regular-text"></td>
+					</tr>
+					</tbody>
+				</table>
+				</td></tr>';
+			}
+		} else {?>
+			<span><?php esc_html_e( 'No Itineraries found.', 'wp-travel' ); ?></span><?php
+		}?>
+
+
+
 		<table style="width:100%">
 			<tr>
 				<td>
