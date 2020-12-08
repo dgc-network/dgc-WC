6faaea1ac1f21dd9753dbd00744185f1be163ea6
roverchen <rover.k.chen@gmail.com>
2020-12-08 12:01:56 +0800

    $trip_data['trip']['faqs'] = $faqs;

--- includes/trip-options-edit-metabox.php
+++ includes/trip-options-edit-metabox.php
@@ -792,10 +792,11 @@ class Trip_Options_Edit_Metabox {
 			if ($_POST['faq_item_question-' . $x] != "" && $_POST['faq_item_question-' . $x] != DEFAULT_QUESTION) {
 				$faqs['question'][$x] = $_POST['faq_item_question-' . $x];
 				$faqs['answer'][$x] = $_POST['faq_item_answer-' . $x];
-				$xx = 0;
+				$xx++;
 			}
 		}
-		$trip_data['trip']['faqs'] = $faqs;
+		//$trip_data['trip']['faqs'] = $faqs;
+		$trip_data->faqs = $faqs;
 		//$question = isset( $faqs['question'] ) ? $faqs['question'] : array();
 		//$answer   = isset( $faqs['answer'] ) ? $faqs['answer'] : array();
 		//update_post_meta( $post_id, 'wp_travel_faq_question', $question );
