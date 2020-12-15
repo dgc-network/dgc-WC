fd4a8f751542429fd7dbd1982fb6b5d521c5ca1e
Rover <rover@MacBook-Pro.local>
2020-12-15 22:25:33 +0800

    <td> 										<select id="itinerary_item_robots" name="itinerary_item_robots-' . $x . '"> 											<option value="">Home Stay ...</option> 											<option value="index,follow"' . selected( 'index,follow', $itineraries[$x]['label'], false ) . '>Show for search engines</option> 											<option value="noindex,nofollow"' . selected( 'noindex,nofollow', $itineraries[$x]['label'], false ) . '>Hide for search engines</option> 										</select> 									</td>

--- includes/trip-options-edit-metabox.php
+++ includes/trip-options-edit-metabox.php
@@ -353,13 +353,25 @@ class Trip_Options_Edit_Metabox {
 							<textarea rows="3" name="itinerary_item_desc-' . $x . '">' . esc_attr( $itineraries[$x]['desc'] ) . '</textarea></td>
 						</tr>
 						<tr>
-							<th>Itinerary robots</th>
-							<td>
-								<select id="itinerary_item_robots" name="itinerary_item_robots-' . $x . '">
-									<option value="">Home Stay ...</option>
-									<option value="index,follow"' . selected( 'index,follow', $itineraries[$x]['label'], false ) . '>Show for search engines</option>
-									<option value="noindex,nofollow"' . selected( 'noindex,nofollow', $itineraries[$x]['label'], false ) . '>Hide for search engines</option>
-								</select>
+							<td colspan="2"><b>' . __( 'Resources Assignment', 'wp-travel' ) .'</b>
+							<table style="width:100%>
+								<tr>
+									<td>
+										<select id="itinerary_item_robots" name="itinerary_item_robots-' . $x . '">
+											<option value="">Home Stay ...</option>
+											<option value="index,follow"' . selected( 'index,follow', $itineraries[$x]['label'], false ) . '>Show for search engines</option>
+											<option value="noindex,nofollow"' . selected( 'noindex,nofollow', $itineraries[$x]['label'], false ) . '>Hide for search engines</option>
+										</select>
+									</td>
+									<td>
+										<select id="itinerary_item_robots" name="itinerary_item_robots-' . $x . '">
+											<option value="">Home Stay ...</option>
+											<option value="index,follow"' . selected( 'index,follow', $itineraries[$x]['label'], false ) . '>Show for search engines</option>
+											<option value="noindex,nofollow"' . selected( 'noindex,nofollow', $itineraries[$x]['label'], false ) . '>Hide for search engines</option>
+										</select>
+									</td>
+								</tr>
+							</table>
 							</td>
 						</tr>
 						<tr>
