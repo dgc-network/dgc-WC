5c9a3a54060001420a307dd144ab25f6f3403b0b
roverchen <rover.k.chen@gmail.com>
2020-12-31 14:45:08 +0800

    valueSelected

--- includes/trip-options-edit-metabox.php
+++ includes/trip-options-edit-metabox.php
@@ -198,7 +198,8 @@ class Trip_Options_Edit_Metabox {
 							var valueSelected = this.value;
 							var opt_categorias = $( '.opt-categorias', sub_element ).val();
 							var opt_tipo = $( '.opt_tipo', sub_element ).val();
-							alert(opt_tipo);
+							//alert(opt_tipo);
+							alert(valueSelected);
 							$( '.opt_tipo', sub_element ).empty();
                 			$( '.opt_tipo', sub_element ).append("<option value=''>- Select Resource -</option>");
                 			$.each(data, function (i, item) {
