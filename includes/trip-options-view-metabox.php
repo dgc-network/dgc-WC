d8841f62c1aeafcc3353e52554575fdbc1e8d191
Rover <rover@MacBook-Pro.local>
2021-01-10 21:21:45 +0800

    $.datepicker.setDefaults( options )

--- includes/trip-options-view-metabox.php
+++ includes/trip-options-view-metabox.php
@@ -83,9 +83,7 @@ class Trip_Options_View_Metabox {
 		?>
 		<script>
 			jQuery(document).ready(function($) {
-				$( '.start_date' ).datepicker({
-					showButtonPanel: true
-				});
+				$( '.start_date' ).datepicker('show');
 				$( '.start_date' ).on( 'change', function() {
 					var start_date = this.value;
 					$( '.itinerary-li' ).each( function( index, element ) {
