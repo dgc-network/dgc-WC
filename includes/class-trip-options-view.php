e1970aab6b2494f5e74c98bf5fa70298a08ca0a1
roverchen <rover.k.chen@gmail.com>
2021-02-04 15:34:01 +0800

    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );

--- includes/class-trip-options-view.php
+++ includes/class-trip-options-view.php
@@ -24,21 +24,26 @@ class Trip_Options_View {
 		add_filter( 'woocommerce_email_recipient_new_booking', array( __CLASS__, 'additional_customer_email_recipient' ), 10, 2 ); 
 		add_filter( 'woocommerce_email_recipient_new_order', array( __CLASS__, 'additional_customer_email_recipient' ), 10, 2 ); // Optional (testing)
 		
-		//add_action( 'woocommerce_after_shop_loop_item', array( __CLASS__, 'remove_add_to_cart_buttons' ), 1 );
+		add_action( 'woocommerce_after_shop_loop_item', array( __CLASS__, 'remove_add_to_cart_buttons' ), 1 );
+		add_action( 'woocommerce_single_product_summary', array( __CLASS__, 'remove_add_to_cart_buttons_from_related' ), 1 );
+
 		//remove_action( 'woocommerce_after_shop_loop_item', array( __CLASS__, 'woocommerce_template_loop_add_to_cart' ) );
 		//remove_action( 'woocommerce_single_product_summary', array( __CLASS__, 'woocommerce_template_single_add_to_cart' ), 30 );
 
 		//remove_action( 'woocommerce_after_shop_loop_item', array( __CLASS__, 'woocommerce_template_loop_add_to_cart' ) );
 		//remove_action( 'woocommerce_single_product_summary', array( __CLASS__, 'woocommerce_template_single_add_to_cart' ), 30 );
 		
-		add_filter( 'woocommerce_is_purchasable', '__return_false');
+		//add_filter( 'woocommerce_is_purchasable', '__return_false');
 	}
 
     function remove_add_to_cart_buttons() {
-      	if( is_product_category() || is_shop()) { 
-        	remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
-			remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
-		}
+		if( is_product_category() || is_shop()) { 
+		  	remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
+	  	}
+	}
+
+    function remove_add_to_cart_buttons_from_related() {
+		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
 	}
 /*
 	function woocommerce_template_single_add_to_cart() {
@@ -55,6 +60,16 @@ class Trip_Options_View {
 	function woocommerce_template_single_add_to_cart() {
     	global $product;
     	do_action( 'woocommerce_' . $product->product_type . '_add_to_cart' );
+	}
+
+	function WC() {
+ 
+		remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart');
+		 
+		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart');
+		 
+		return WooCommerce::instance();
+		 
 	}
 
 	function custom_datepicker() {
