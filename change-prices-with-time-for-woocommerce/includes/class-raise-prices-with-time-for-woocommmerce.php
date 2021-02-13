2e390fd185401d10f6084149be70ba3e5abe9924
Rover <rover@MacBook-Pro.local>
2021-02-13 23:23:19 +0800

    payment_cashback_product_data

--- change-prices-with-time-for-woocommerce/includes/class-raise-prices-with-time-for-woocommmerce.php
+++ change-prices-with-time-for-woocommerce/includes/class-raise-prices-with-time-for-woocommmerce.php
@@ -179,7 +179,7 @@ if ( ! class_exists( 'Raise_Prices_With_Time_For_Woocommmerce' ) ) {
 		private function define_public_hooks() {
 
 			$plugin_public = new Raise_Prices_With_Time_For_Woocommmerce_Public( $this->get_plugin_name(), $this->get_version() );
-
+/*
 			$this->loader->add_action( 'init', $plugin_public, 'register_shortcodes' );
 			$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 			$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
@@ -190,7 +190,7 @@ if ( ! class_exists( 'Raise_Prices_With_Time_For_Woocommmerce' ) ) {
 			$this->loader->add_filter( 'woocommerce_single_product_summary', $plugin_public, 'show_single_product_countdown', 11 );
 			$this->loader->add_filter( 'woocommerce_add_to_cart', $plugin_public, 'add_cart_item_data', 99 );
 			$this->loader->add_filter( 'woocommerce_get_cart_item_from_session', $plugin_public, 'load_cart_item_data_from_session', 5, 2 );
-
+*/
 
 			if ( 'yes' === get_option( 'cpwt_show_countdown_on_shop_pages', 'no' ) ) {
 				$this->loader->add_action( 'woocommerce_after_shop_loop_item', $plugin_public, 'show_product_countdown_loop', 10 );
