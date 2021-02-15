6156302373124061ae1ed22671babb71e9f896e4
Rover <rover@MacBook-Pro.local>
2021-02-15 09:28:36 +0800

    //wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/trip-options-admin.css', array(), $this->version, 'all' );

--- includes/class-trip-options-admin.php
+++ includes/class-trip-options-admin.php
@@ -160,6 +160,7 @@ class Trip_Options_Admin {
 	public function enqueue_styles() {
 
 		//wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/trip-options-admin.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->plugin_name, 'css/trip-options-admin.css', array(), $this->version, 'all' );
 
 	}
 
