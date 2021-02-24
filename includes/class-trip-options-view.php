81c7b637666b0c7063bd9eedde285f23d0e292ed
roverchen <rover.k.chen@gmail.com>
2021-02-24 14:48:16 +0800

    var from = $( "#from" )     .datepicker({           defaultDate: "+1w",           changeMonth: true,     })     .on( "change", function() {           to.datepicker( "option", "minDate", getDate( this ) );     });
        var to = $( "#to" ).datepicker({
            defaultDate: "+1w",
            changeMonth: true,
        })
        .on( "change", function() {
            from.datepicker( "option", "maxDate", getDate( this ) );
        });
    
        function getDate( element ) {
            var date;
            try {
                date = $.datepicker.parseDate( dateFormat, element.value );
            } catch( error ) {
                date = null;
            }
            return date;
        }

--- includes/class-trip-options-view.php
+++ includes/class-trip-options-view.php
@@ -187,6 +187,33 @@ class Trip_Options_View {
 			jQuery(document).ready(function($) {
     			//var dateFormat = "mm/dd/yy",
     			//var dateFormat = "Y-m-d",
+				var from = $( "#from" )
+    .datepicker({
+          defaultDate: "+1w",
+          changeMonth: true,
+    })
+    .on( "change", function() {
+          to.datepicker( "option", "minDate", getDate( this ) );
+    });
+
+    var to = $( "#to" ).datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+    })
+    .on( "change", function() {
+        from.datepicker( "option", "maxDate", getDate( this ) );
+    });
+
+    function getDate( element ) {
+        var date;
+        try {
+            date = $.datepicker.parseDate( dateFormat, element.value );
+        } catch( error ) {
+            date = null;
+        }
+        return date;
+    }
+
   			} );
   			</script>
 			<?php
