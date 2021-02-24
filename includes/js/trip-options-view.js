eda0cb40f4b900e4a2254a5bb31c9d70a63f08c6
roverchen <rover.k.chen@gmail.com>
2021-02-24 16:43:17 +0800

    $( 'input', element2 ).val(updated_trip_date.toLocaleDateString());

--- includes/js/trip-options-view.js
+++ includes/js/trip-options-view.js
@@ -73,7 +73,8 @@ jQuery(document).ready(function($) {
         //$( '#start_date_input' ).val(updated_start_date.toLocaleDateString());
         $( '.itinerary-li' ).each( function( index, element ) {
             updated_start_date.setDate(start_date.getDate() + index);
-            $( 'input', element ).val(updated_start_date.toLocaleDateString());
+            //$( 'input', element ).val(updated_start_date.toLocaleDateString());
+            $( 'input', element ).val(updated_start_date.toString());
             $( '#itinerary-date-'+index ).datepicker();
             $( '#itinerary-date-'+index ).on( 'change', function() {
                 var trip_date = new Date(this.value);
@@ -81,7 +82,8 @@ jQuery(document).ready(function($) {
                 $( '.itinerary-li' ).each( function( index2, element2 ) {
                     if (index2 > index) {
                         updated_trip_date.setDate(trip_date.getDate() + index2 - index);
-                        $( 'input', element2 ).val(updated_trip_date.toLocaleDateString());
+                        //$( 'input', element2 ).val(updated_trip_date.toLocaleDateString());
+                        $( 'input', element2 ).val(updated_trip_date.toString());
                     }
                 });
             });
